e93f0f07ca3d6d3cc68561a591965ed95840f341
Austin Lowe <babaganoush45@gmail.com>
2019-06-13 14:36:06 -0400

    clean up, and added switch logic

---
+++ Logic/SwitchProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DataLibrary.Models;
+using DataLibrary.DataAccess;
+
+namespace DataLibrary.Logic
+{
+    class SwitchProcessor
+    {
+        public static List<SwitchModel> LoadSwitches()
+        {
+            string sql = @"select HOSTNAME, DEVICE_TYPE, IP, USERNAME, PASSWORD, SECRET
+                            from dbo.SWITCH;";
+
+            return SQLDataAccess.LoadData<SwitchModel>(sql);
+        }
+    }
+}
